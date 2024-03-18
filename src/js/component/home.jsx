6878815aba8b226a6a9a3f557b3704c3bd2ec961
asalam3312 <141695644+asalam3312@users.noreply.github.com>
2024-03-18 21:56:41 +0000

    in progress

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,106 @@
-import React from "react";
-
+import React, { useState } from "react";
+import homestyles from "./homestyles.css"
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+
 
 //create your first component
 const Home = () => {
+
+	const [redColor, setRedColor] = useState(false);
+	/* const [yellowColor, setYellowColor]= useState(false);
+	const [greenColor, setGreenColor]= useState(false); */
+
+	/* 	const handleClick =()=>{
+			setRedColor ? redLightOn : redLightOff
+		} */
+	const handleClick = () => {
+		setRedColor(!redColor); 
+	};
+
+	const redLightOn = () => {
+		<div className="d-flex flex-column text-center border border-dark">
+			<button type="button" id="redOn"></button>
+		</div>
+	}
+
+	const redLightOff = () => {
+		<div className="d-flex flex-column text-center border border-dark">
+			<button type="button" id="red"></button>
+		</div>
+	}
+
+	const redLight = () => {
+		if (redColor) {
+			return redLightOn(); 
+		} else {
+			return redLightOff(); 
+		}
+	};
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div>
+	{redLight()}
+			<button className="d-flex flex-column text-center border border-dark" onClick={handleClick}></button>
 		</div>
 	);
 };
 
 export default Home;
+/* const yellowLight =()=>{
+	if(yellowLight=false){
+		<div className="d-flex flex-column text-center border border-dark">
+			<button type="button" id="yellow" oneClick={handleClick}></button>
+		</div>
+	}{
+		<div className="d-flex flex-column text-center border border-dark">
+			<button type="button" id="yellowOn" oneClick={handleClick}></button>
+		</div>
+	}
+};
+const greenlight =()=>{
+	if(greenlight=false){
+		<div className="d-flex flex-column text-center border border-dark">
+			<button type="button" id="green" oneClick={handleClick}></button>
+		</div>
+	}{
+		<div className="d-flex flex-column text-center border border-dark">
+			<button type="button" id="greenOn" oneClick={handleClick}></button>
+		</div>
+	}
+}; */
+
+
+
+/* 	
+return(
+	<div>
+		<div className="d-flex flex-column text-center border border-dark">
+				<button type="button" id="red" oneClick={handleClick}></button>
+			</div>
+			<div className="d-flex flex-column text-center border border-dark">
+				<button type="button" id="redOn" oneClick={handleClick}></button>
+			</div>
+
+			<div className="d-flex flex-column text-center border border-dark">
+				<button type="button" id="yellow" oneClick={handleClick}></button>
+			</div>
+			<div className="d-flex flex-column text-center border border-dark">
+				<button type="button" id="yellowOn" oneClick={handleClick}></button>
+			</div>
+
+			<div className="d-flex flex-column text-center border border-dark">
+				<button type="button" id="green" oneClick={handleClick}></button>
+			</div>
+			<div className="d-flex flex-column text-center border border-dark">
+				<button type="button" id="greenOn" oneClick={handleClick}></button>
+			</div>
+			<button onClick={change}></button>
+	</div>
+) */
+
+
+
+
+
+
+
