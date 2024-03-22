88f882512fc43a7988c6113f6c848be439d2610a
asalam3312 <141695644+asalam3312@users.noreply.github.com>
2024-03-22 16:38:18 +0000

    purpleLightIn and button added successfully

--- src/js/component/TrafficLight.jsx
+++ src/js/component/TrafficLight.jsx
@@ -3,70 +3,77 @@ import homestyles from "./homestyles.css"
 
 const TrafficLight = () => {
 
-  const [redColor, setRedColor] = useState(false);
+	const [redColor, setRedColor] = useState(false);
 	const [yellowColor, setYellowColor] = useState(false);
 	const [greenColor, setGreenColor] = useState(false);
+	const [purpleColor, setPurpleColor] = useState(false);
 
 	//color handlers
 	const handleClickR = () => {
 		setRedColor(!redColor);
+		setYellowColor(false);
+		setGreenColor(false)
 	};
 	const handleClickY = () => {
 		setYellowColor(!yellowColor);
+		setRedColor(false);
+		setGreenColor(false);
 	};
 	const handleClickG = () => {
 		setGreenColor(!greenColor);
+		setRedColor(false);
+		setYellowColor(false);
 	};
+	const handleClickP = () => {
+		setPurpleColor(!purpleColor);
+		setRedColor(false);
+		setYellowColor(false);
+		setGreenColor(false);
+	}
+
 
 	//const color lights on or not
+
 	const redLightOn = () => {
 		return (
-			<div id="redOn">
-				<div className="btn-danger" type="button"></div>
-			</div>
+			<div id="redOn"></div>
 		)
 
 	}
 
 	const redLightOff = () => {
 		return (
-			<div id="red">
-				<div className="btn-warning" type="button" ></div>
-			</div>
+			<div id="red"></div>
 		)
 	}
 
 	const yellowLightOn = () => {
 		return (
-			<div id="yellowOn">
-				<div className="btn-danger" type="button"></div>
-			</div>
+			<div id="yellowOn"></div>
 		)
 
 	}
 	const yellowLightOff = () => {
 		return (
-			<div id="yellow">
-				<div className="btn-warning" type="button" ></div>
-			</div>
+			<div id="yellow"></div>
 		)
 	}
 	const greenLightOn = () => {
 		return (
-			<div id="greenOn">
-				<div className="btn-danger" type="button"></div>
-			</div>
+			<div id="greenOn"></div>
 		)
 
 	}
 	const greenLightOff = () => {
 		return (
-			<div id="green">
-				<div className="btn-warning" type="button" ></div>
-			</div>
+			<div id="green"></div>
 		)
 	}
-
+	const purpleLightIn = () => {
+		return (
+			<div id="purple"></div>
+		)
+	}
 	//conditionals applied to each light
 	const redLight = () => {
 		if (redColor) {
@@ -90,15 +97,29 @@ const TrafficLight = () => {
 		}
 	};
 
-	return (
-		<div>
-			<div className="father rounded">
-				<div className="d-flex justify-content-center" onClick={handleClickR}>{redLight()}</div>
-				<div className="d-flex justify-content-center" onClick={handleClickY}>{yellowLight()}</div>
-				<div className="d-flex justify-content-center" onClick={handleClickG}>{greenLight()}</div>
+	const trafficLightReady = () => {
+		return (
+			<div className='mt-3'>
+				<div className="father rounded mt-3">
+					<div className="d-flex justify-content-center" onClick={handleClickR}>{redLight()}</div>
+					<div className="d-flex justify-content-center" onClick={handleClickY}>{yellowLight()}</div>
+					<div className="d-flex justify-content-center" onClick={handleClickG}>{greenLight()}</div>
+					<div className="d-flex justify-content-center" >{purpleColor ? <div></div> : <div>{purpleLightIn()}</div>}</div>
+				</div>
+				<div className="post">
+
+				</div>
+				<div className="position-absolute top-50 start-50 translate-middle mt-5">
+					<div>
+						<button className="ms-1 mt-5" id="buttonInPost" onClick={handleClickP}></button>
+					</div>
+				</div>
 			</div>
-			<div className="post"></div>
-		</div>
+		)
+	}
+	return (
+
+		<div>{trafficLightReady()}</div>
 
 	);
 };
